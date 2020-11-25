479604658626002bc0e0349ef88549f2422e5243
Slaven B <slaven.bunijevac@gmail.com>
2020-11-25 23:42:41 +0100

    restart button functionality

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const start = document.querySelector('#start');
 const stop = document.querySelector('#stop');
 const reset = document.querySelector('#reset');
+const restart = document.querySelector('#restart');
 
 let startTime = 0;
-let myTimer;
-start.addEventListener('click', () => {
+let myTimer, timesOnStop;
+start.addEventListener('click', e => {
+  // e.target.setAttribute('disabled', 'true');
   startTime = new Date().getTime();
   myTimer = setInterval(() => {
     let newTime = new Date().getTime();
@@ -15,19 +17,26 @@ start.addEventListener('click', () => {
 
 stop.addEventListener('click', () => {
   clearInterval(myTimer);
-  const stopTime = new Date().getTime();
-  const differenceInHundreds = (Math.floor((stopTime - time) / 10)) % 100;
-  const differenceInSeconds = (Math.floor((stopTime - time) / 1000)) % 60;
-  const differenceInMinutes = Math.floor((stopTime - time) / 1000 / 60);
-  hundredsElement.innerText = differenceInHundreds > 9 ? `: ${differenceInHundreds}` : `: 0${differenceInHundreds}`;
-  secondsElement.innerText = differenceInSeconds > 9 ? `: ${differenceInSeconds}` : `: 0${differenceInSeconds}`;
-  minutesElement.innerText = differenceInSeconds > 9 ? `${differenceInMinutes}` : `0${differenceInMinutes}`;
-  });
-
-reset.addEventListener('click', () => {
-  time = 0;
+  stopTime = new Date().getTime();
+  timesOnStop = createTimes(startTime, stopTime);
+  // output.innerText = outputTimes(timesOnStop);
 });
 
+reset.addEventListener('click', () => {
+  clearInterval(myTimer);
+  startTime = 0;
+  output.innerText = `00 : 00 : 00`;
+});
+
+restart.addEventListener('click', () => {
+  const restartTime = new Date().getTime();
+  myTimer = setInterval(() => {
+    const newTime = new Date().getTime();
+    const restartTimes = createTimes(restartTime, newTime);
+    const totalTimes = addRestartTimes(timesOnStop, restartTimes);
+    output.innerText = outputTimes(totalTimes);
+  }, 100);
+});
 
 const createTimes = (startTime, endTime) => {
   return {
@@ -42,4 +51,12 @@ const outputTimes = ({ hundreds, minutes, seconds }) => {
   const secondsText = seconds > 9 ? `: ${seconds} ` : `: 0${seconds} `;
   const minutesText = minutes > 9 ? `${minutes} ` : `0${minutes} `;
   return minutesText + secondsText + hundredsText;
+}
+
+const addRestartTimes = (stopTimes, restartTimes) => {
+  return {
+    hundreds: (stopTimes.hundreds + restartTimes.hundreds) % 100,
+    seconds: (stopTimes.seconds + restartTimes.seconds) % 60,
+    minutes: stopTimes.minutes + restartTimes.minutes
+  }
 }
